cffdfbc4dada4ee40d3e9bd41f9f62a6ef6c1e1b
Li Chaoran <409624189@qq.com>
2016-03-22 11:30:06 +0800

    fix not install vimrc when using vim

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ if [ ! -z "`which nvim`" ];then
 	mv ~/.config/nvim ~/.config/nvim`date +%Y%m%d`
 	cp -r nvim ~/.config/
 else
-	mv ~/.vim ~/.vim`date +%Y%m%d`
+	[ -d ~/.vim ] && mv ~/.vim ~/.vim`date +%Y%m%d`
+	[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc`date +%Y%m%d`
 	cp -r nvim ~/.vim
+	mv ~/.vim/init.vim ~/.vimrc
 fi
