946b25d304cf4ee13c0510ded1ac1c376cfada15
Li Chaoran <pkwarcraft@gmail.com>
2016-12-22 21:24:54 +0800

    sync from upstream

--- start_steam.sh
+++ start_steam.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' nohup /usr/bin/steam &
+LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' nohup /usr/bin/steam > /tmp/nohub_steam &
